ecfcb9fb117ecc3763bbd3696b51d64ac7eca882
Gui-Boulet <140693886+Gui-Boulet@users.noreply.github.com>
2023-11-22 21:52:19 +0100

    Add files via upload

--- public/assets/admin.js
+++ public/assets/admin.js
@@ -1,8 +1,45 @@
+/* ******************************************************* */
+/* Déclarations des constantes - Declarations of constants */
+/* ******************************************************* */
+
+// Ciblage des différentes fenêtres modales - Targeting the different modal
 const customerModal = document.getElementById('customerModal')
 const messageModal = document.getElementById('messageModal')
 const imageModal = document.getElementById('imageModal')
 const deleteModal = document.getElementById('deleteModal')
 
+
+/* ****************************************************** */
+/* Déclarations des fonctions - Declarations of functions */
+/* ****************************************************** */
+
+// Valide les champs d'un formulaire - Validates fields on a form
+function validateField(field, regex) {
+  const valueField = field.value.trim()
+
+  if (!regex.test(valueField)) {
+    field.classList.add('is-invalid')
+    return false
+  }
+  return true
+}
+
+// Affiche un message d'erreur sous un champ de formulaire lorsque la saisie est incorrecte
+// Displays an error message under a form field when the entry is incorrect
+function displayErrorMessage(isValid, message, key, field) {
+  if (!isValid[key]) {
+    message[key].classList.remove('d-none')
+    message[key].classList.add('d-block')
+    setTimeout(function() {
+      message[key].classList.remove('d-block')
+      message[key].classList.add('d-none')
+    }, 10000)
+  } else {
+    field[key].classList.remove('is-invalid')
+    message[key].classList.remove('d-block')
+    message[key].classList.add('d-none')
+  }
+}
 
 // Retourne les données à insérer selon la fenêtre modale - Returns the data to be inserted according to the modal
 function loadModal(typeModal){
@@ -20,8 +57,8 @@ function loadModal(typeModal){
             // Extracting information from data-customer-id attributes
             const containerCustomerModal = {"id" : button.getAttribute('data-customer-id')}
             
-            // Ciblage des champs à modifier dans la fenêtre modale customerModal
-            // Targeting fields to edit in the customerModal modal
+            // Ciblage des champs à remplir dans la fenêtre modale customerModal
+            // Targeting fillable fields in the customerModal modal
             const areaCustomerModal = {
               "id" : typeModal.querySelector('#customer-user_id'),
               "name" : typeModal.querySelector('#customer-name'),
@@ -36,19 +73,61 @@ function loadModal(typeModal){
 
             // Mis à jour du contenu de la fenêtre modale customerModal
             // Updated the contents of the customerModal modal
-            for (let i = 0; i < datasCustomer.length; i++){
-              if (datasCustomer[i]['id'] == containerCustomerModal['id']) {
-                areaCustomerModal['id'].value = containerCustomerModal['id']
-                areaCustomerModal['name'].textContent = datasCustomer[i]['name']
-                areaCustomerModal['gardenSize'].querySelector(`option[value="${datasCustomer[i]['gardenSize']}"]`).selected = true
-                areaCustomerModal['hedgeLength'].value = datasCustomer[i]['hedgeLength']
-                areaCustomerModal['fruitTree'].value = datasCustomer[i]['fruitTree']
-                areaCustomerModal['shrub'].value = datasCustomer[i]['shrub']
-                areaCustomerModal['smallTree'].value = datasCustomer[i]['smallTree']
-                areaCustomerModal['bigTree'].value = datasCustomer[i]['bigTree']
-                areaCustomerModal['note'].value = datasCustomer[i]['note']
+            for (let i = 0; i < datasCustomer.length; i++) {
+              if (datasCustomer[i].id == containerCustomerModal.id) {
+                areaCustomerModal.id.value = containerCustomerModal.id
+                areaCustomerModal.name.textContent = datasCustomer[i].name
+                areaCustomerModal.gardenSize.querySelector(`option[value="${datasCustomer[i].gardenSize}"]`).selected = true
+                areaCustomerModal.hedgeLength.value = datasCustomer[i].hedgeLength
+                areaCustomerModal.fruitTree.value = datasCustomer[i].fruitTree
+                areaCustomerModal.shrub.value = datasCustomer[i].shrub
+                areaCustomerModal.smallTree.value = datasCustomer[i].smallTree
+                areaCustomerModal.bigTree.value = datasCustomer[i].bigTree
+                areaCustomerModal.note.value = datasCustomer[i].note
               }
             }
+            
+            const errorMessage = {
+              "hedgeLength" : typeModal.querySelector('#customer-hedge_length-error'),
+              "fruitTree" : typeModal.querySelector('#customer-fruit_tree-error'),
+              "shrub" : typeModal.querySelector('#customer-shrub-error'),
+              "smallTree" : typeModal.querySelector('#customer-small_tree-error'),
+              "bigTree" : typeModal.querySelector('#customer-big_tree-error'),
+              "note" : typeModal.querySelector('#customer-note-error')
+            }
+
+            const buttonSubmit = typeModal.querySelector('#customer-submit')
+
+            buttonSubmit.addEventListener('click', function() {
+              const isFieldValid = {
+                "hedgeLength" : validateField(areaCustomerModal.hedgeLength,
+                  /^(0|[1-9]|[1-9][0-9]|[1-9][0-9]{2}|[1][0-9]{3}|2000)$/),
+                "fruitTree" : validateField(areaCustomerModal.fruitTree,
+                  /^(0|[1-9]|[1-4][0-9]|50)$/),
+                "shrub" : validateField(areaCustomerModal.shrub,
+                  /^(0|[1-9]|[1-4][0-9]|50)$/),
+                "smallTree" : validateField(areaCustomerModal.smallTree,
+                  /^(0|[1-9]|[1-4][0-9]|50)$/),
+                "bigTree" : validateField(areaCustomerModal.bigTree,
+                  /^(0|[1-9]|[1-4][0-9]|50)$/),
+                "note" : validateField(areaCustomerModal.note,
+                  /^[^<>"';&/\\]{0,250}$/)
+              }
+
+              displayErrorMessage(isFieldValid, errorMessage, 'hedgeLength', areaCustomerModal)
+              displayErrorMessage(isFieldValid, errorMessage, 'fruitTree', areaCustomerModal)
+              displayErrorMessage(isFieldValid, errorMessage, 'shrub', areaCustomerModal)
+              displayErrorMessage(isFieldValid, errorMessage, 'smallTree', areaCustomerModal)
+              displayErrorMessage(isFieldValid, errorMessage, 'bigTree', areaCustomerModal)
+              displayErrorMessage(isFieldValid, errorMessage, 'note', areaCustomerModal)
+
+              if (!(isFieldValid.hedgeLength && isFieldValid.fruitTree && isFieldValid.shrub && isFieldValid.smallTree
+                  && isFieldValid.bigTree && isFieldValid.note)) {
+                buttonSubmit.setAttribute('type', 'button')
+              } else {
+                buttonSubmit.setAttribute('type', 'submit')
+              }
+            })
             break
 
           
@@ -62,8 +141,8 @@ function loadModal(typeModal){
               "comment" : button.getAttribute('data-message-comment')
             }
               
-            // Ciblage des champs à modifier dans la fenêtre modale messageModal
-            // Targeting fields to edit in the messageModal modal
+            // Ciblage des champs à remplir dans la fenêtre modale messageModal
+            // Targeting fillable fields in the messageModal modal
             const areaMessageModal = {
               "name" : typeModal.querySelector('#customer-name'),
               "date" : typeModal.querySelector('#message-date'),
@@ -72,9 +151,9 @@ function loadModal(typeModal){
 
             // Mis à jour du contenu de la fenêtre modale messageModal
             // Updated the contents of the messageModal modal
-            areaMessageModal['name'].textContent = containerMessageModal['name']
-            areaMessageModal['date'].textContent = containerMessageModal['date']
-            areaMessageModal['comment'].textContent = containerMessageModal['comment']
+            areaMessageModal.name.textContent = containerMessageModal.name
+            areaMessageModal.date.textContent = containerMessageModal.date
+            areaMessageModal.comment.textContent = containerMessageModal.comment
             break
 
           
@@ -87,14 +166,14 @@ function loadModal(typeModal){
               "fileName" : button.getAttribute('data-service-fileName')
             }
 
-            // Ciblage des champs à modifier dans la fenêtre modale imageModal
-            // Targeting fields to edit in the imageModal modal
+            // Ciblage du champ à remplir dans la fenêtre modale imageModal
+            // Targeting the field to be filled in the imageModal modal
             const areaImageModal = typeModal.querySelector('#service-image')
       
             // Mis à jour du contenu de la fenêtre modale imageModal
             // Updated the contents of the imageModal modal
-            areaImageModal.setAttribute('alt', containerImageModal['name'])
-            areaImageModal.setAttribute('src', `./images/${containerImageModal['fileName']}.jpg`)
+            areaImageModal.setAttribute('alt', containerImageModal.name)
+            areaImageModal.setAttribute('src', `./images/${containerImageModal.fileName}.jpg`)
             break
 
           
@@ -107,8 +186,8 @@ function loadModal(typeModal){
               'name' : button.getAttribute('data-service-name')
             }
 
-            // Ciblage des champs à modifier dans la fenêtre modale deleteModal
-            // Targeting fields to edit in the deleteModal modal
+            // Ciblage des champs à remplir dans la fenêtre modale deleteModal
+            // Targeting fillable fields in the deleteModal modal
             const areaDeleteModal = {
               'id' : typeModal.querySelector('#service-id'),
               'name' : typeModal.querySelector('#service-name')
@@ -116,8 +195,8 @@ function loadModal(typeModal){
       
             // Mis à jour du contenu de la fenêtre modale deleteModal
             // Updated the contents of the deleteModal modal
-            areaDeleteModal['id'].value = containerDeleteModal['id']
-            areaDeleteModal['name'].textContent = containerDeleteModal['name']
+            areaDeleteModal.id.value = containerDeleteModal.id
+            areaDeleteModal.name.textContent = containerDeleteModal.name
             break
         }
       }
@@ -125,16 +204,19 @@ function loadModal(typeModal){
   }
 }
 
+
+/* *************************************** */
+/* Appel des fonctions - Calling functions */
+/* *************************************** */
+
+// Chargement des données dans les différentes fenêtres modales - Loading data into different modal
 loadModal(customerModal)
 loadModal(messageModal)
 loadModal(imageModal)
 loadModal(deleteModal)
 
-// Affiche un élément pendant 5 sec - Displays an item for 5 sec
-function timeOut(element){
-  setTimeout(function() {
-    document.getElementById(element).style.display = 'none';
-  }, 5000)
-}
-
-timeOut('checkIcon')
+// Affichage temporaire d'un icône de validation - Temporary display of a validation icon
+setTimeout(function() {
+  document.getElementById('checkIcon').classList.remove('d-block')
+  document.getElementById('checkIcon').classList.add('d-none')
+}, 10000)
--- public/index.php
+++ public/index.php
@@ -5,11 +5,14 @@ use App\Router;
 # Inclu le fichier autoload.php
 require  dirname(__DIR__) . '/vendor/autoload.php';
 
-
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
 
 $router = new Router(dirname(__DIR__) . '/views');
 $router
   ->get('/', 'access/index', 'home')
+  ->post('/actions-insert', 'access/actions/insert', 'actions_insert')
   ->get('/login', 'access/login', 'login')
   ->get('/account', 'user/account', 'account')
   ->get('/admin-interventions', 'admin/interventions', 'admin_interventions')
